a22b5ed6a2f60da5c455377f955dea5a88c10be6
Celia_Betourne <Celia.betourne@gmail.com>
2021-08-24 21:31:53 +0200

    proto_mixed scene

---
+++ LAV_InteractiveInstallation/Assets/Kinect_Data.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kinect_Data : MonoBehaviour
+{
+    public GameObject Hip;
+ /*   public float Value;
+    public Vector3 HipXValue;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Value = Hip.transform.position.x;
+
+        if (Value != 0)
+        {
+            HipXValue = Hip.transform.position;
+            Debug.Log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        }
+    }
+    */
+}
